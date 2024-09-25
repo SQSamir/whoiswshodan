55e2b2e101b49178ad6a4cda481051a8126957ba
SQSamir <SQSamir@users.noreply.github.com>
2024-09-25 17:35:55 +0200

    Has been updated by GPT

--- whois_w_shodan.py
+++ whois_w_shodan.py
@@ -3,7 +3,7 @@
 """
 __author__ = "SQS"
 
-extract domain names  from ssl certificate
+Extract domain names from SSL certificate
 """
 
 import socket
@@ -11,85 +11,96 @@ import ssl
 import sys
 import shodan
 import re
-global SHODAN_API_KEY
-global api
-SHODAN_API_KEY="paste here" #Get free API from https://shodan.io
-api=shodan.Shodan(SHODAN_API_KEY)
+
+# Constants
+SHODAN_API_KEY = "paste_here"  # Get free API from https://shodan.io
+api = shodan.Shodan(SHODAN_API_KEY)
 
 
 def extract(domainname):
+    """Extracts subdomains from the SSL certificate of the provided domain"""
     cont = ssl.create_default_context()
     conn = cont.wrap_socket(socket.socket(socket.AF_INET), server_hostname=domainname)
+    
     try:
         conn.connect((domainname, 443))
-    except  (ssl.CertificateError,ssl.SSLError,socket.gaierror) as e:
-            print("Error :", e)
-            sys.exit()
-    cert = conn.getpeercert()
-    subdomains = (cert['subjectAltName'])
-    dmn=[]
+        cert = conn.getpeercert()
+        conn.close()  # Always close the socket
+    except (ssl.CertificateError, ssl.SSLError, socket.gaierror, ConnectionRefusedError) as e:
+        print(f"Error while connecting to {domainname}: {e}")
+        sys.exit()
+
+    if 'subjectAltName' not in cert:
+        print(f"No subjectAltName found in certificate for {domainname}")
+        return []
+
+    subdomains = cert['subjectAltName']
+    dmn = []
+
     for i, domain in subdomains:
-        if "*" in domain:
-            wstar = (re.split(r'[*]', domain))
-            lstar=str(wstar[1])
-            dmn.append(lstar[1:])
-        elif domain in dmn:
-            pass
-        else:
+        if "*" in domain:  # Handle wildcard domains
+            cleaned_domain = domain.replace("*.", "")
+            if cleaned_domain not in dmn:
+                dmn.append(cleaned_domain)
+        elif domain not in dmn:
             dmn.append(domain)
+
     return dmn
 
 
-
-
 def axtar(keysoz):
-    esas={'country_name':'Ölkə :', 'city':'Şəhər :', 'hostname':'Hostname :', 'org':'Təşkilat :', 'isp':'ISP :', 'ip_str':'IP :'}
+    """Queries Shodan for information related to the IP address"""
+    esas = {
+        'country_name': 'Ölkə :',
+        'city': 'Şəhər :',
+        'hostname': 'Hostname :',
+        'org': 'Təşkilat :',
+        'isp': 'ISP :',
+        'ip_str': 'IP :'
+    }
 
     try:
-
-        c=api.host(keysoz)
-        for key in c:
-            if key in esas:
-                print(esas[key], c[key])
-
-            elif key =='data':
-                print( "Açıq portlar:")
-                for  ind in range(len(c['ports'])):
-                    print( c[key][ind]['port'],":", c[key][ind]['transport'])
-                print ("-------------------------------")
+        c = api.host(keysoz)
+        for key in esas:
+            if key in c:
+                print(f"{esas[key]} {c[key]}")
+        
+        # Check for open ports and transport details
+        if 'data' in c and 'ports' in c:
+            print("Açıq portlar:")
+            for ind, port_info in enumerate(c['data']):
+                print(f"{port_info['port']} : {port_info['transport']}")
+            print("-------------------------------")
     except shodan.APIError as e:
-        print('Error:', e)
-
-
-
+        print(f'Shodan API Error: {e}')
 
 
 def resolver(domain):
+    """Resolves the domain name to its corresponding IP address"""
     try:
-        ip=socket.gethostbyname(domain)
+        ip = socket.gethostbyname(domain)
         return ip
-    except:
-        pass
-
-
-
+    except socket.gaierror:
+        print(f"Failed to resolve {domain}")
+        return None
 
 
 def main():
-    for sdomain in extract(soz):
+    soz = sys.argv[1] if len(sys.argv) > 1 else None
+    if not soz:
+        print(f"Usage: {sys.argv[0]} <domain>")
+        sys.exit()
 
-        ipaddr=resolver(sdomain)
-        print("\n"+sdomain+"\n")
-        if ipaddr is None:
-            pass
+    for sdomain in extract(soz):
+        print(f"\nDomain: {sdomain}\n")
+        ipaddr = resolver(sdomain)
+
+        if ipaddr:
+            print(f"Resolved IP: {ipaddr}")
+            axtar(ipaddr)
         else:
-            b=(axtar(ipaddr))
-            if b != None:
-                 print(b)
-try:
-    soz=sys.argv[1]
-except:
-    print("Usage: "+sys.argv[0]+" example.com")
-    sys.exit()
+            print(f"Could not resolve domain: {sdomain}")
+
+
 if __name__ == '__main__':
     main()
